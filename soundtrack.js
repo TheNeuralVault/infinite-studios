6423eb60e9559691e395394ba9d63572189bfa17
Jonathan Battles <aiexperimentlab101@gmail.com>
2025-12-13 06:05:21 -0600

    Update soundtrack.js

--- soundtrack.js
+++ soundtrack.js
@@ -1,8 +1,3 @@
-/**
- * ARTIFACT: NEURAL SYNTH
- * Protocol: Web Audio API Procedural Score
- */
-
 export class Soundtrack {
     constructor() {
         this.ctx = null;
@@ -12,13 +7,9 @@ export class Soundtrack {
     init() {
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         this.ctx = new AudioContext();
-        
-        // Master Bus
         this.master = this.ctx.createGain();
         this.master.gain.value = 0.0;
         this.master.connect(this.ctx.destination);
-
-        // Underwater Filter
         this.filter = this.ctx.createBiquadFilter();
         this.filter.type = 'lowpass';
         this.filter.frequency.value = 200;
@@ -28,35 +19,35 @@ export class Soundtrack {
     startDrone() {
         if (!this.ctx) this.init();
         
-        // The "Blade Runner" Chord (Root, 5th, Octave)
-        const freqs = [55, 82.41, 110, 164.81]; 
-        
+        // FORCE RESUME FOR ANDROID
+        if (this.ctx.state === 'suspended') {
+            this.ctx.resume();
+        }
+
+        const freqs = [55, 82.41, 110]; 
         freqs.forEach(f => {
             const osc = this.ctx.createOscillator();
-            osc.type = 'sawtooth'; // Gritty sound
+            osc.type = 'sawtooth';
             osc.frequency.value = f;
-            
-            // LFO for movement (The "Throb")
             const lfo = this.ctx.createOscillator();
-            lfo.frequency.value = 0.1 + Math.random() * 0.2;
+            lfo.frequency.value = 0.1;
             const lfoGain = this.ctx.createGain();
-            lfoGain.gain.value = 150;
+            lfoGain.gain.value = 100;
             lfo.connect(lfoGain);
             lfoGain.connect(this.filter.frequency);
-            
             osc.connect(this.filter);
             osc.start();
             lfo.start();
         });
 
-        // Slow Fade In
-        this.master.gain.setTargetAtTime(0.5, this.ctx.currentTime, 10);
+        this.master.gain.setTargetAtTime(0.4, this.ctx.currentTime, 5);
     }
 
     swell() {
         if(!this.ctx) return;
-        // Open the filter briefly on scene change
-        this.filter.frequency.setTargetAtTime(700, this.ctx.currentTime, 0.5);
+        if (this.ctx.state === 'suspended') this.ctx.resume();
+        
+        this.filter.frequency.setTargetAtTime(600, this.ctx.currentTime, 0.5);
         setTimeout(() => {
             this.filter.frequency.setTargetAtTime(200, this.ctx.currentTime, 4);
         }, 500);
